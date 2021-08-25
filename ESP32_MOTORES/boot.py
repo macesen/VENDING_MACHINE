33c1c3efea51875ef23b185eb90ceea74c91b703
macesen <70594406+macesen@users.noreply.github.com>
2021-08-25 17:30:48 -0500

    Create boot.py

---
+++ ESP32_MOTORES/boot.py
@@ -0,0 +1,61 @@
+import time
+from time import sleep,sleep_ms
+from umqttsimple import MQTTClient
+import ubinascii
+import machine
+import micropython
+from machine import Pin,PWM
+import network
+import esp
+import utime
+esp.osdebug(None)
+import gc
+gc.collect()
+
+#CONEXION Y CLAVE DE INTERNET----------------------------------------------------------------
+ssid = 'NETLIFE-C-B'
+password = '0602092686'
+#CONEXION AL BROKER, SIEMPRE USAR 'broker.emqx.io'--------------------------------------------
+mqtt_server = 'broker.emqx.io'
+client_id = ubinascii.hexlify(machine.unique_id())
+#CREACION DE SUSCRIPTORES---------------------------------------------------------------------
+topic_pub = b'boton_1'
+#CREACION DE PUBLICADORES---------------------------------------------------------------------
+topic_sub = b'boton_3'
+#---------------------------------------------------------------------------------------------
+
+station = network.WLAN(network.STA_IF)
+
+station.active(True)
+station.connect(ssid, password)
+#CONEXION CON LA  RED------------------------------------------------------------------------
+while station.isconnected() == False:
+  pass
+
+print('Connection successful')
+print(station.ifconfig())   
+'''
+led_1=Pin(19,Pin.OUT)
+
+
+led_2=Pin(18,Pin.OUT)
+
+
+led_3=Pin(5,Pin.OUT)
+
+
+boton_1=Pin(26,Pin.IN)
+'''
+#SERVOS
+servo_360_1 = PWM(Pin(17))
+servo_360_1.freq(50)
+
+
+servo_360_2 = PWM(Pin(16))
+servo_360_2.freq(50)
+
+
+servo_360_3 = PWM(Pin(5))
+servo_360_3.freq(50)  
+# PIR
+pir=Pin(14,Pin.IN)
